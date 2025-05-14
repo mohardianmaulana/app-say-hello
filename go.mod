e22055c75951a76f64d72d06b76be3f6326bfee0
unknown <ardi320042004@gmail.com>
2025-05-14 08:52:59 +0700

    update-major

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/mohardianmaulana/app-say-hello
 
 go 1.24.2
 
-require github.com/mohardianmaulana/go-say-hello v1.0.0 // indirect
+require github.com/mohardianmaulana/go-say-hello/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/mohardianmaulana/go-say-hello"
+	"github.com/mohardianmaulana/go-say-hello/v2"
 )
 
 func main() {
-	fmt.Println(go_say_hello.SayHello())
+	fmt.Println(go_say_hello.SayHello("Ardi"))
 }
